90bdc1f6d91d246a170588670acbff6644c91486
juan <flowkilero13@outlook.es>
2023-04-02 16:07:58 -0500

    my sixth commit

--- Ejercicie 11-15/prueba-pseint-7-4.js
+++ Ejercicie 11-15/prueba-pseint-7-4.js
@@ -1,3 +1,16 @@
+(function () {
+  function changeWindow(x) {
+    if (x.matches) {
+      console.log(true);
+      document.querySelector('#p-btn').classList.add('col-12', 'btn-group');
+    } else {
+      document.querySelector('#p-btn').classList.remove('col-12', 'btn-group');
+    }
+  }
+  let x = window.matchMedia("(max-width: 768px)");
+  changeWindow(x);
+  x.addEventListener("change", changeWindow);
+})();
 window.onload = function () {
   document.getElementById('formCalc').reset();
 }
@@ -7,7 +20,7 @@ formCalc.addEventListener('submit', (e) => {
   e.preventDefault();
   // Ejercicies 14
   let numCalc = parseFloat(document.getElementById('numCalc').value);
-  let calcCube = Number((6 * Math.pow(numCalc, 2)));
+  let calcCube = Number(6 * (numCalc**2));
   demo.textContent = (isNaN(calcCube) ? 'Error' : calcCube);
   e.target.reset();
 });
